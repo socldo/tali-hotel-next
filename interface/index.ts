4102bcaddd28293120427265852405b857630e45
Tai <dalatndc@gmail.com>
2023-07-12 09:10:48 +0700

    no message

--- interface/index.ts
+++ interface/index.ts
@@ -1,37 +1,37 @@
-declare namespace Model {
+// declare namespace Model {
 
 
-    interface Branch {
-        id: number;
-        name: string;
-        email: string;
-        address: string;
-        phone: string;
-        status: boolean;
-        images: string;
-    }
+//     interface Branch {
+//         id: number;
+//         name: string;
+//         email: string;
+//         address: string;
+//         phone: string;
+//         status: boolean;
+//         images: string;
+//     }
 
 
-    interface APIResponse {
-        status: number;
-        message: string;
-        data: Branch[];
-    }
+//     interface APIResponse {
+//         status: number;
+//         message: string;
+//         data: Branch[];
+//     }
 
-    interface User {
-        id: number;
-        role_id: number;
-        birthday: string;
-        avatar: string;
-        name: string;
-        email: string;
-        address: string;
-        phone: string;
-        gender: number;
-        is_locked: boolean;
-        created_at: string;
-        updated_at: string;
-    }
+//     interface User {
+//         id: number;
+//         role_id: number;
+//         birthday: string;
+//         avatar: string;
+//         name: string;
+//         email: string;
+//         address: string;
+//         phone: string;
+//         gender: number;
+//         is_locked: boolean;
+//         created_at: string;
+//         updated_at: string;
+//     }
 
 
-}
+// }
