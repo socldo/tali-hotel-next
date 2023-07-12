f1ca96509ebb2c4c41c08d7f1b909fda362878bc
Tai <dalatndc@gmail.com>
2023-07-12 10:10:49 +0700

    no message

--- services/index.ts
+++ services/index.ts
@@ -1,8 +1,6 @@
 import { hotelApi } from './hotelApi'
-import { roomApi } from './roomApi'
-import { userApi } from './userApi'
 import { uploadApi } from './uploadApi'
 import { bookingApi } from './bookingApi'
 import { authApi } from './authApi'
 
-export { hotelApi, roomApi, authApi, userApi, uploadApi, bookingApi }
+export { hotelApi, authApi, uploadApi, bookingApi }
